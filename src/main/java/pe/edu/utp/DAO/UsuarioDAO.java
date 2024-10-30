5632450c936149b418c0e8beec5dd0b7eb827bd8
paolomarxel <paolomarx36@gmail.com>
2024-10-30 00:35:02 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/utp/DAO/UsuarioDAO.java
@@ -0,0 +1,15 @@
+package pe.edu.utp.DAO;
+
+import pe.edu.utp.Model.Cliente;
+import pe.edu.utp.Model.Usuario;
+import java.util.List;
+
+public interface UsuarioDAO {
+
+    Usuario obtenerUsuario(String username, String password);
+    Usuario obtenerUsuarioPorId(int idUsuario);
+    boolean registrarCajero(Cliente cliente, Usuario user);
+    List<Usuario> listarUsuariosCajeros();
+    void actualizarUsuarioCajero(Usuario usuario);
+    void eliminarUsuarioCajero(int idUsuario);
+}
